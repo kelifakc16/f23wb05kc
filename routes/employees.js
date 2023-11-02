caeb48949b4ca74680e0138411ba6f317da8d350
kelifakc16 <kelifakc@gmail.com>
2023-11-01 21:05:07 -0500

    bonus

--- routes/employees.js
+++ routes/employees.js
@@ -2,13 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-var results = [
-    { employeeID: '123', officeLocation: 'Office A', yearsOfService: 5 },
-    { employeeID: '456', officeLocation: 'Office B', yearsOfService: 3 },
-    { employeeID: '789', officeLocation: 'Office C', yearsOfService: 7 }
-  ];
+
 router.get('/', function(req, res, next) {
-    res.render('employees', { title: 'Employees List', results: results });
+    res.render('employees', { title: 'Employees List' });
 });
 
 module.exports = router;
